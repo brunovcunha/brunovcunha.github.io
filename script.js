05a7841e97ccac32d87f432f131fa41774a469c1
brunovcunha <133446390+brunovcunha@users.noreply.github.com>
2023-06-16 20:30:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function decimalParaBinario(decimal) {
+    return parseInt(decimal).toString(2);
+}
+function binarioParaDecimal(binario) {
+    var decimal = 0;
+    var posicao = 0;
+    while (binario > 0) {
+        var digito = binario % 10;
+        decimal += digito * Math.pow(2, posicao);
+        binario = Math.floor(binario / 10);
+        posicao++;
+
+    }
+    return decimal;
+}
